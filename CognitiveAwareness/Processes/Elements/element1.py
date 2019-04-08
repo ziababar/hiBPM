935bb54fad6687225bfd21841db44089886deec8
Zia Babar <zbabar@gmail.com>
2019-04-08 15:46:30 -0400

    updated templates

---
+++ CognitiveAwareness/Processes/Elements/element1.py
@@ -0,0 +1,38 @@
+##
+## Element Template
+## 
+
+# Process Stages
+# Process Phases
+# Process Elements
+# Relationships between Process Stages. Relationships can have 
+#   - type
+#   - data
+#   - label
+#   - plans (should map to plan)
+#   - designs (should map to design)
+#   - start node (should map to process stage)
+#   - termination node (should map to process stage)
+#   - 
+
+# A process element performs the following
+#  - takes in an input(s)
+#  - processes it
+#  - generates a single output.
+
+# A process stage ...
+#
+
+# A process phase ...
+
+# Process Architecture
+#  - define the structure of process stages and relationships
+#     - linkages
+#     - inputs and ouputs
+#     - goal attainment
+#  - define the placement of PEs in PSs or PPs
+#  - 
+
+# A process element belongs to a process stage, or process phase.
+# The process stage is a collection of process stages
+# Their execution sequence is specified
---
+++ CognitiveAwareness/Processes/process1.py
@@ -0,0 +1,27 @@
+
+##
+## Context Sensing and Determining
+##
+
+# Sense Context
+# Replan Based on Context
+# Reconfigure BPA
+
+##
+## Domain Business Process
+##
+
+# Process Loan Application
+# Setup Loan Repayment
+# Repay Loan
+
+##
+## Analytical Modeling
+##
+
+# Build Business Understanding
+# Business Improvement
+# Develop Algorithm Catalogue
+# Define Data Preparation
+# Determine Analytical Design
+# Create Analytical Model
