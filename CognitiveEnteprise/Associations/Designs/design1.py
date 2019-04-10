630ee385223b37c55edb2cc737276ecb61fd94cf
ziababar <zbabar@gmail.com>
2019-04-09 23:10:01 -0400

    design type

--- CognitiveEnteprise/Associations/Designs/design1.py
+++ CognitiveEnteprise/Associations/Designs/design1.py
@@ -1,3 +1,4 @@
 
 # Pattern Repository. This component stores a collection of solution patterns. Patterns can be expressed in standard, machine understandable formats (e.g., XML) so that the content can be queried and retrieved at run-time.
 
+# This is a design
